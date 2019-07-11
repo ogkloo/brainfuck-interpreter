998c916ddfbbee691d18967db00036e0fc5f9286
ogkloo <no@email.net>
2019-07-11 13:17:13 -0700

    Initial functionality without text processing.

---
+++ src/main.rs
@@ -0,0 +1,132 @@
+/// List of brainfuck instructions
+/// As detailed in https://esolangs.org/wiki/Brainfuck
+#[derive(Debug, PartialEq)]
+pub enum Instruction {
+    Add,
+    Sub,
+    Right,
+    Left,
+    Branch,
+    End,
+    Print,
+    Input,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Tape {
+    storage: Vec<(i8, usize)>,
+    position: usize,
+    branches: Vec<usize>,
+    instructions: Vec<Instruction>,
+    instruction_pointer: usize,
+}
+
+impl Tape {
+    pub fn run(&mut self) {
+        while self.handle_instruction() {
+            println!("{:?}", self);
+        }
+    }
+
+    pub fn handle_instruction(&mut self) -> bool {
+        if self.instruction_pointer >= self.instructions.len() {
+            false
+        } else {
+            match self.instructions[self.instruction_pointer] {
+                Instruction::Add => {
+                    match self
+                        .storage
+                        .iter()
+                        .position(|&elem| elem.1 == self.position)
+                    {
+                        Some(index) => self.storage[index].0 += 1,
+                        None => self.storage.push((1, self.position)),
+                    }
+                    self.instruction_pointer += 1;
+                }
+                Instruction::Sub => {
+                    match self
+                        .storage
+                        .iter()
+                        .position(|&elem| elem.1 == self.position)
+                    {
+                        Some(index) => self.storage[index].0 -= 1,
+                        None => self.storage.push((-1, self.position)),
+                    }
+                    self.instruction_pointer += 1;
+                }
+                Instruction::Right => {
+                    self.position += 1;
+                    self.instruction_pointer += 1;
+                }
+                Instruction::Left => {
+                    if self.position >= 1 {
+                        self.position -= 1;
+                    }
+                    self.instruction_pointer += 1;
+                }
+                Instruction::Print => {
+                    match self
+                        .storage
+                        .iter()
+                        .position(|&elem| elem.1 == self.position)
+                    {
+                        Some(index) => println!("{}", self.storage[index].0),
+                        None => println!("0"),
+                    }
+                    self.instruction_pointer += 1;
+                }
+                Instruction::Input => {
+                    self.instruction_pointer += 1;
+                }
+                Instruction::Branch => {
+                    self.branches.push(self.instruction_pointer);
+                    self.instruction_pointer += 1;
+                }
+                Instruction::End => {
+                    match self
+                        .storage
+                        .iter()
+                        .position(|&elem| elem.1 == self.position)
+                    {
+                        Some(index) => {
+                            if self.storage[index].0 == 0 {
+                                match self.branches.pop() {
+                                    Some(branch) => self.instruction_pointer = branch,
+                                    None => panic!("Jump to non-existent branch statement."),
+                                }
+                            } else {
+                                self.instruction_pointer += 1;
+                            }
+                        }
+                        None => match self.branches.pop() {
+                            Some(branch) => self.instruction_pointer = branch,
+                            None => panic!("Jump to non-existent branch statement."),
+                        },
+                    }
+                }
+            }
+            true
+        }
+    }
+}
+
+fn main() {
+    // Infinite loop program
+    let mut tape = Tape {
+        storage: Vec::new(),
+        position: 0,
+        branches: Vec::new(),
+        instructions: vec![
+            Instruction::Branch,
+            Instruction::Print,
+            Instruction::Add,
+            Instruction::Right,
+            Instruction::Add,
+            Instruction::Sub,
+            Instruction::End,
+        ],
+        instruction_pointer: 0,
+    };
+    tape.run();
+}
